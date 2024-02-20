fbd079dd8506a91994a53b37e88300bf819ae5d7
Maarthaii <maar.thaii.2003@gmail.com>
2024-02-20 16:22:13 -0500

    Cors

--- backend/src/server/index.ts
+++ backend/src/server/index.ts
@@ -3,9 +3,12 @@ import express, {
 } from 'express'
 import router from '../routes'
 import morgan from 'morgan'
+import cors from 'cors'
 
 const server: Express = express()
 
+server.use(cors)
+server.use(express.json())
 server.use(morgan('dev'))
 
 server.use('/', router)
