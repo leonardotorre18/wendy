e3eb9df6246f6a630f42f80edcd2ab3e16cf2c08
Leonardotorre18 <Leonardotorre14@gmail.com>
2024-02-24 16:33:43 -0400

    Backend: Swagger installed and configured

--- backend/src/server/index.ts
+++ backend/src/server/index.ts
@@ -6,12 +6,25 @@ import morgan from 'morgan'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import '../database/index'
+import swaggerUi from 'swagger-ui-express'
+import swaggerJSDoc from 'swagger-jsdoc'
+import swaggerOptions from '../swagger/config'
+
 dotenv.config()
+
 const server: Express = express()
 
 server.use(cors())
+// server.use(express.urlencoded())
 server.use(express.json())
 server.use(morgan('dev'))
+
+server.use('/docs',
+  swaggerUi.serve,
+  swaggerUi.setup(
+    swaggerJSDoc(swaggerOptions)
+  )
+)
 
 server.use('/', router)
 
