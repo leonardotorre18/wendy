2aa177d9feb86cbf043cb1342c6db3f513fe4f5d
Leonardotorre18 <Leonardotorre14@gmail.com>
2024-03-16 19:26:50 -0400

    Backend: Hashing password in logins and registers

--- backend/src/controllers/Doctors.controller.ts
+++ backend/src/controllers/Doctors.controller.ts
@@ -1,27 +1,36 @@
 import { type Request, type Response } from 'express'
 import { createDoctor } from '../models/orm/DoctorsORM'
 import { type IDoctor } from '../models/interfaces/IDoctor'
+import { hashSync } from 'bcryptjs'
+import { type IPatient } from '../models/interfaces/IPatient'
 
 export class DoctorsController {
   public register = (req: Request, res: Response): void => {
-    const { name, email, password, age } = req.body
-    createDoctor({
-      name,
-      email: email.toString().toLocaleUpperCase(),
-      password,
-      age,
-      specialties: [],
-      availability: []
-    })
-      .then((doctor: IDoctor) => {
-        res.status(201).json({
-          doctor
-        })
+    const { name, email, age, password }: IPatient = req.body
+
+    if (password !== undefined && password.length >= 8) {
+      createDoctor({
+        name,
+        email: email.toString().toLocaleUpperCase(),
+        password: hashSync(password),
+        age,
+        specialties: [],
+        availability: []
       })
-      .catch(() => {
-        res.status(401).json({
-          err: 'Error'
+        .then((doctor: IDoctor) => {
+          res.status(201).json({
+            doctor
+          })
         })
+        .catch(() => {
+          res.status(401).json({
+            err: 'Error'
+          })
+        })
+    } else {
+      res.status(401).json({
+        err: 'Error'
       })
+    }
   }
 }
--- backend/src/controllers/Patients.controller.ts
+++ backend/src/controllers/Patients.controller.ts
@@ -1,25 +1,34 @@
 import { type Request, type Response } from 'express'
 import { createPatient } from '../models/orm/PatientsORM'
 import { type IPatient } from '../models/interfaces/IPatient'
+import { type IDoctor } from '../models/interfaces/IDoctor'
+import { hashSync } from 'bcryptjs'
 
 export class PatientsController {
   public register = (req: Request, res: Response): void => {
-    const { name, email, password, age } = req.body
-    createPatient({
-      name,
-      email: email.toString().toLocaleUpperCase(),
-      password,
-      age
-    })
-      .then((doctor: IPatient) => {
-        res.status(201).json({
-          doctor
-        })
+    const { name, email, password, age }: IDoctor = req.body
+
+    if (password !== undefined && password.length >= 8) {
+      createPatient({
+        name,
+        email: email.toString().toLocaleUpperCase(),
+        password: hashSync(password),
+        age
       })
-      .catch(() => {
-        res.status(401).json({
-          err: 'Error'
+        .then((doctor: IPatient) => {
+          res.status(201).json({
+            doctor
+          })
         })
+        .catch(() => {
+          res.status(401).json({
+            err: 'Error'
+          })
+        })
+    } else {
+      res.status(401).json({
+        err: 'Error'
       })
+    }
   }
 }
